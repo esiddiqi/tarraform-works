e3f7320d21bdb8e9827a813b2f1fe71b7bfb7023
esiddiqi <siddiquiemaad@gmail.com>
2022-04-03 15:12:20 +0530

    Created Instance config

---
+++ first_instance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+   region = "us-east-2"
+}
+
+resource "aws_instance" "first_instance" {
+      ami = "ami-0c02fb55956c7d316"
+      instance_type = "t2.micro"
+      availability_zone = "us-east-2a"
+      key_name = vprofile-bean-key
+      vpc_security_group_ids ["sg-0067ecb2e827a7934"]
+      tags = {
+         Name = "tarra_instance"
+         }
+
+}
