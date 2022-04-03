9ea2b724893d52ba210b7c8b1e4035c0fe5a5d9d
esiddiqi <siddiquiemaad@gmail.com>
2022-04-03 15:18:00 +0530

    Created Instance config

--- first_instance.tf
+++ first_instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "first_instance" {
-      ami = "ami-0c02fb55956c7d316"
+      ami = "ami-064ff912f78e3e561"
       instance_type = "t2.micro"
       availability_zone = "us-east-2a"
       key_name = "vprofile-bean-key"
