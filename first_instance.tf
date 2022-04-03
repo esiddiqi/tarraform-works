3034dccae3fdd39143c9ea17074a1ee8fc2cd692
esiddiqi <siddiquiemaad@gmail.com>
2022-04-03 15:24:22 +0530

    Created Instance config

--- first_instance.tf
+++ first_instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "first_instance" {
       ami = "ami-064ff912f78e3e561"
       instance_type = "t2.micro"
       availability_zone = "us-east-2a"
-      key_name = "vprofile-bean-key"
+      key_name = "ansible-ec2-ohio"
       vpc_security_group_ids = ["sg-05ed6f021b22c2359"]
       tags = {
          Name = "tarra_instance"
